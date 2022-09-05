b27a2d120588d861cc3d56700a44144ea562c82a
Lauro-Santos <32677114+Lauro-Santos@users.noreply.github.com>
2022-09-05 20:04:31 -0300

    atualizacao

---
+++ README.md
@@ -0,0 +1,1 @@
+# Lauro Santos - Portifolio
---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+//alterador de skills (ainda tem que melhorar)
+const progressoSkill_html = document.querySelector('#skil_htlm');
+const progressoSkill_css = document.querySelector('#skil_css');
+const progressoSkill_javaScript = document.querySelector('#skil_javaScript');
+const progressoSkill_react = document.querySelector('#skil_react');
+const progressoSkill_ui = document.querySelector('#skil_ui');
+
+progressoSkill_html.setAttribute("style", "width: 75%;");
+progressoSkill_css.setAttribute("style", "width: 60%;");
+progressoSkill_javaScript.setAttribute("style", "width: 50%;");
+progressoSkill_react.setAttribute("style", "width: 8%;");
+progressoSkill_ui.setAttribute("style", "width: 80%;");

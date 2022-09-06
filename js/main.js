7e91db4a172860613d8e39071b0dbe4468a04f3b
Lauro-Santos <32677114+Lauro-Santos@users.noreply.github.com>
2022-09-06 20:08:26 -0300

    footer finishing

--- js/main.js
+++ js/main.js
@@ -27,3 +27,15 @@ function fixarCabecalhoTopo() {
 window.onscroll = function() {
     fixarCabecalhoTopo();
 }
+
+const botao = document.querySelector('.botaoCopiar');
+const email = document.querySelector('.email');
+
+botao.addEventListener('click', copiarEmail);
+
+function copiarEmail() {
+    
+    navigator.clipboard.writeText(email.innerText);
+    alert("Entre em contato e vamos trabalhar juntos!");
+    
+}

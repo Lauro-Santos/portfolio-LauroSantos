4293c9032460c13ccc6e4274c62c77f8c0ccc281
Lauro-Santos <32677114+Lauro-Santos@users.noreply.github.com>
2022-09-05 21:47:39 -0300

    fixed header attempt

--- js/main.js
+++ js/main.js
@@ -11,3 +11,17 @@ progressoSkill_css.setAttribute("style", "width: 60%;");
 progressoSkill_javaScript.setAttribute("style", "width: 50%;");
 progressoSkill_react.setAttribute("style", "width: 8%;");
 progressoSkill_ui.setAttribute("style", "width: 80%;");
+
+
+//nao esta funcionando
+const cabecalho = document.getElementsByName("nav")[0];
+const cabecalhoTop = cabecalho.offsetTop();
+
+
+function fixarCabecalhoTopo() {
+    if (window.pageYOffset >= cabecalhoTop) {
+        cabecalho.classList.add("cabecalho--fixed");
+    } else {
+        cabecalho.classList.remove("cabecalho--fixed");
+    }
+}
